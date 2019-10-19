32ac8d0e42cc0ae434ff6228bfea60a3f3b38b31
Roman <r_truba@i.ua>
2019-10-19 17:07:54 +0200

    converted RegisterController & Login Controller

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,7 +3,11 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use Dotenv\Exception\ValidationException;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Foundation\Auth\ThrottlesLogins;
+use Illuminate\Http\Request;
+use Auth;
 
 class LoginController extends Controller
 {
@@ -18,22 +22,63 @@ class LoginController extends Controller
     |
     */
 
-    use AuthenticatesUsers;
+    use ThrottlesLogins;
 
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
-    protected $redirectTo = '/cabinet';
 
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
     }
+
+    public function showLoginForm()
+    {
+        return view('auth.login');
+    }
+
+
+    public function login(Request $request)
+    {
+       $this->velidate($request,[
+           'email' => 'required|string',
+           'password' => 'required|string'
+       ]);
+
+       if ($this->hasTooManyLoginAttempts($request)){
+           $this->fireLockoutEvent($request);
+           $this->sendLockoutResponse($request);
+       }
+
+       $authentificate = Auth::attempt(
+           $request->only(['email','password']),
+           $request->filled('remember')
+       );
+
+       if ($authentificate) {
+           $request->session()->regenerate();
+           $this->clearLoginAttempts($request);
+           $user = Auth::user();
+           if (!$user->status !== User::STATUS_ACTIVE) {
+                Auth::logout();
+                return back()->with('error','You need to confirm your account. Pleace check your email');
+           }
+           return redirect()->intended(route('cabinet'));
+       }
+
+       $this->incrementLoginAttempts($request);
+
+       throw ValidationException::withMessages(['email' => [trans('auth.failed')]]);
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::guard()->logout();
+        $request->session()->invalidate();
+        return redirect()->route('home');
+    }
+
+    protected function username()
+    {
+        return 'name';
+    }
+
 }
--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Mail\VerifyMail;
 use App\User;
+use Illuminate\Auth\Events\Registered;
 use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
@@ -40,33 +43,58 @@ class RegisterController extends Controller
         $this->middleware('guest');
     }
 
-    /**
-     * Get a validator for an incoming registration request.
-     *
-     * @param  array  $data
-     * @return \Illuminate\Contracts\Validation\Validator
-     */
-    protected function validator(array $data)
+    public function showRegistrationForm()
     {
-        return Validator::make($data, [
+        return view('auth.register');
+    }
+
+
+    public function register(Request $request)
+    {
+        $this->validate($request,[
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
+        $user = User::create([
+            'name' => $request['name'],
+            'email' => $request['email'],
+            'password' => Hash::make($request['password']),
+            'verify_token' => \Str::random(),
+            'status' => User::STATUS_WAIT,
+        ]);
+
+        \Mail::to($user->email)->send(new VerifyMail($user));
+        event(new Registered($user));
+
+        return redirect()->route('login')
+            ->with('success','Check your email and click on the link to verify.');
     }
 
-    /**
-     * Create a new user instance after a valid registration.
-     *
-     * @param  array  $data
-     * @return \App\User
-     */
-    protected function create(array $data)
+
+
+
+
+
+
+    public function verify($token)
     {
-        return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => Hash::make($data['password']),
-        ]);
+        if (!$user = User::where('verify_token', $token)->first()) {
+            return redirect()->route('login')
+                ->with('error','Sorry your link cannot be identified.');
+        }
+
+        if($user->status !== User::STATUS_WAIT) {
+            return redirect()->route('login')
+                ->with('error','Your email is alredy verified.');
+        }
+
+        $user->status = User::STATUS_ACTIVE;
+        $user->verify_token = null;
+        $user->save;
+        return redirect()->route('login')
+            ->with('success','Your e-mail is verified. You can now login.');
+
     }
+
 }
