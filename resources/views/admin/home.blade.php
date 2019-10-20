549683e09f636e2d77bfde2da8343cffa10b024e
Roman <r_truba@i.ua>
2019-10-20 12:36:08 +0200

    bread-crumbs for Admin

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app)
+
+@section('content')
+    <ul class="nan nav-tabs mb-3">
+        <li class="nav-item"><a href="{{route('admin.home')}}" class="nav-link active">Dashboard</a></li>
+    </ul>
+    @endsection
